fec982bb187e651af645ed9417a0371967916ed6
gary <gary.field@pobox.com>
2023-05-24 16:02:35 +0100

    Added HTTPS scheme

--- test/ginger.integration.test.ts
+++ test/ginger.integration.test.ts
@@ -1,10 +1,8 @@
 import {expect, test} from '@jest/globals';
 
 test('makes http request', async () => {
-    //process.env.APP_URL = 'wuckertdaughertyharveystammmacgyver.newapp.io'
-    const appUrl= `https://${process.env.APP_URL}`
-    console.log(`APP_URL is ${appUrl}`)
-    const r = await fetch('https://wuckertdaughertyharveystammmacgyver.newapp.io')
+    console.log(`APP_URL=${process.env.APP_URL}`)
+    const r = await fetch(`https://${process.env.APP_URL}`)
     const t = await r.text()
     expect(t).toContain('Hello World')
 
